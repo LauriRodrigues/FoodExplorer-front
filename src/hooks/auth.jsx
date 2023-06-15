06b866b4cbf78bd281669f441e289534af12c833
LauriRodrigues <laurianarodrigueslima@gmail.com>
2023-06-15 19:07:30 -0300

    hook auth att

--- src/hooks/auth.jsx
+++ src/hooks/auth.jsx
@@ -1,6 +1,6 @@
 import { createContext, useContext, useState, useEffect } from "react"
 
-import { api } from '../service/api'
+import { api } from '../services/api'
 
 export const AuthContext = createContext({})
 
@@ -50,7 +50,6 @@ export function AuthProvider({ children }) {
         {children}
     </AuthContext.Provider>
   )
-
 }
 
 export function useAuth() {
