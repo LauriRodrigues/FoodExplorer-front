805c5034d40d6527a9172f3068708462f152b6b9
LauriRodrigues <laurianarodrigueslima@gmail.com>
2023-06-15 19:01:26 -0300

    hook auth

--- src/pages/SignIn/index.jsx
+++ src/pages/SignIn/index.jsx
@@ -1,9 +1,24 @@
+import { useState, useRef } from 'react'
 import { Container, Brand, Form } from './styles'
 import Logo from '../../assets/explorerLogo.svg'
 import { Input } from '../../components/Input'
 import { Button } from '../../components/Button'
+import { useAuth } from '../../hooks/auth'
 
 export function SignIn() {
+  const { signIn } = useAuth()
+  const [email, setEmail] = useState('')
+  const [ password, setPassword ] = useState('')
+
+  const loginButton = useRef(null)
+
+  async function handleSignIn(event) {
+    event.preventDefault();
+    if (!email || !password) return alert('Por favor, informe email e senha!')
+    loginButton.current.disabled = true
+    await signIn({ email, password })
+    loginButton.current.disabled = false
+  }
 
   return (
     <Container>
@@ -19,20 +34,26 @@ export function SignIn() {
           type="email"
           title="Email"
           placeholder="Exemplo: exemplo@exemplo.com.br"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
         />
 
         <Input  
           type="password"
           title="Senha"
           placeholder="No mínimo 6 caracteres"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
         />
 
         <Button 
           type="submit" 
           title="Entrar"
+          ref={loginButton}
+          onClick={handleSignIn}
         />
 
-        <a href="/"> Criar uma conta </a>
+        <a href="/register"> Criar uma conta </a>
     
       </Form>
 
