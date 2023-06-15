41173178439fba8688f853d97d8c7f464884670a
LauriRodrigues <laurianarodrigueslima@gmail.com>
2023-06-15 19:03:53 -0300

    hook auth att

--- src/hooks/auth.jsx
+++ src/hooks/auth.jsx
@@ -6,53 +6,54 @@ export const AuthContext = createContext({})
 
 export function AuthProvider({ children }) {
 
-    const [data, setData] = useState({})
+  const [data, setData] = useState({})
 
-    async function signIn({ email, password }) {
-        try {
-            const response = await api.post("/sessions", {email, password})
-            const { user, token } = response.data
-            localStorage.setItem("@foodExplorer:user", JSON.stringify(user))
-            localStorage.setItem("@foodExplorer:token", token)
-            api.defaults.headers.common['Authorization'] = `Bearer ${token}`
-            setData({user, token})
-            console.log(user, token)
-        }
-
-        catch (error) {
-            if(error.response) {
-                alert(error.response.data.message)
-                console.log(error)
-            } else {
-                alert("Não foi possível entrar.")
-            }
-        }
-    }
-    function signOut() {
-        localStorage.removeItem("@foodExplorer:token")
-        localStorage.removeItem("@foodExplorer:user")
-        setData({})
+  async function signIn({ email, password }) {
+    try {
+      const response = await api.post("/sessions", {email, password})
+      const { user, token } = response.data
+      localStorage.setItem("@foodExplorer:user", JSON.stringify(user))
+      localStorage.setItem("@foodExplorer:token", token)
+      api.defaults.headers.common['Authorization'] = `Bearer ${token}`
+      setData({user, token})
+      console.log(user, token)
     }
 
-    useEffect(() => {
-        const token = localStorage.getItem("@foodExplorer:token")
-        const user = localStorage.getItem("@foodExplorer:user")
+    catch (error) {
+      if(error.response) {
+          alert(error.response.data.message)
+          console.log(error)
+      } else {
+          alert("Não foi possível entrar.")
+      }
+    }
+  }
 
-        if(token && user) {
-            api.defaults.headers.common['Authorization'] = `Bearer ${token}`
-            setData({token, user: JSON.parse(user)})
-        }
-    }, [])
+  function signOut() {
+    localStorage.removeItem("@foodExplorer:token")
+    localStorage.removeItem("@foodExplorer:user")
+    setData({})
+  }
 
-    return (
-        <AuthContext.Provider value={{ signIn, signOut, user: data.user}}>
-            {children}
-        </AuthContext.Provider>
-    )
+  useEffect(() => {
+    const token = localStorage.getItem("@foodExplorer:token")
+    const user = localStorage.getItem("@foodExplorer:user")
+
+    if(token && user) {
+      api.defaults.headers.common['Authorization'] = `Bearer ${token}`
+      setData({token, user: JSON.parse(user)})
+    }
+  }, [])
+
+  return (
+    <AuthContext.Provider value={{ signIn, signOut, user: data.user}}>
+        {children}
+    </AuthContext.Provider>
+  )
 
 }
 
 export function useAuth() {
-    const context = useContext(AuthContext)
-    return context
+  const context = useContext(AuthContext)
+  return context
 }
