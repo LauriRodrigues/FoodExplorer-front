03b5463428ff594efbb7a0b5f07384f2bff6c2c6
LauriRodrigues <laurianarodrigueslima@gmail.com>
2023-07-04 13:43:24 -0300

    ingredient component

---
+++ src/components/Ingredient/styles.js
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+export const Container = styled.span`
+  height: 3.2rem;
+  min-width: fit-content;
+  display: flex;
+  align-items: center;
+  padding: .4rem .8rem;
+  border-radius: .5rem;
+  background-color: ${({ theme }) => theme.COLORS.DARK_100};
+`
